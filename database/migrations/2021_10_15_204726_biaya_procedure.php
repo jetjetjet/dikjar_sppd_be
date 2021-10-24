73ec11af78e39fcf85cd6e932499cf5514d2991b
afdal <afd.zik@gmail.com>
2021-10-24 20:51:08 +0700

    commend sp

--- database/migrations/2021_10_15_204726_biaya_procedure.php
+++ database/migrations/2021_10_15_204726_biaya_procedure.php
@@ -13,62 +13,62 @@ class BiayaProcedure extends Migration
      */
     public function up()
     {
-        $procedure = "DROP FUNCTION IF EXISTS biaya_grid;
-        CREATE FUNCTION biaya_grid( p_biaya_id integer, p_pegawai_id integer)
-        RETURNS TABLE(reference_id bigint, modul text, tipe text, nama varchar, tanggal date, keterangan text, biaya numeric, remark varchar, created timestamp, file bigint)
-        AS $$
-        BEGIN
-            return query select * from 
-            ( 
-              select id,
-                'PENGELUARAN',
-                'lainnya' as jenis, 
-                kategori, 
-                tgl as tanggal, jml || ' ' || satuan as jml, 
-                total, 
-                catatan, 
-                created_at,
-                file_id
-              from pengeluaran p 
-              where deleted_at is null
-              and biaya_id = p_biaya_id
-              and pegawai_id = p_pegawai_id
-              union all 
-              select id,
-                'INAP',
-                'Penginapan', 
-                hotel, 
-                tgl_checkin, 
-                jml_hari || ' Hari', 
-                total_bayar, 
-                catatan, 
-                created_at,
-                file_id
-              from inap i 
-              where deleted_at is null 
-              and biaya_id = p_biaya_id
-              and pegawai_id = p_pegawai_id
-              union all 
-              select id,
-                'TRANSPORT',
-                jenis_transport, 
-                agen, 
-                tgl, 
-                perjalanan, 
-                total_bayar, 
-                catatan, 
-                created_at,
-                file_id
-              from transport t 
-              where deleted_at is null
-              and biaya_id = p_biaya_id
-              and pegawai_id = p_pegawai_id 
-            ) a
-            order by a.created_at desc;
-        end;
-        $$LANGUAGE plpgsql;";
+        // $procedure = "DROP FUNCTION IF EXISTS biaya_grid;
+        // CREATE FUNCTION biaya_grid( p_biaya_id integer, p_pegawai_id integer)
+        // RETURNS TABLE(reference_id bigint, modul text, tipe text, nama varchar, tanggal date, keterangan text, biaya numeric, remark varchar, created timestamp, file bigint)
+        // AS $$
+        // BEGIN
+        //     return query select * from 
+        //     ( 
+        //       select id,
+        //         'PENGELUARAN',
+        //         'lainnya' as jenis, 
+        //         kategori, 
+        //         tgl as tanggal, jml || ' ' || satuan as jml, 
+        //         total, 
+        //         catatan, 
+        //         created_at,
+        //         file_id
+        //       from pengeluaran p 
+        //       where deleted_at is null
+        //       and biaya_id = p_biaya_id
+        //       and pegawai_id = p_pegawai_id
+        //       union all 
+        //       select id,
+        //         'INAP',
+        //         'Penginapan', 
+        //         hotel, 
+        //         tgl_checkin, 
+        //         jml_hari || ' Hari', 
+        //         total_bayar, 
+        //         catatan, 
+        //         created_at,
+        //         file_id
+        //       from inap i 
+        //       where deleted_at is null 
+        //       and biaya_id = p_biaya_id
+        //       and pegawai_id = p_pegawai_id
+        //       union all 
+        //       select id,
+        //         'TRANSPORT',
+        //         jenis_transport, 
+        //         agen, 
+        //         tgl, 
+        //         perjalanan, 
+        //         total_bayar, 
+        //         catatan, 
+        //         created_at,
+        //         file_id
+        //       from transport t 
+        //       where deleted_at is null
+        //       and biaya_id = p_biaya_id
+        //       and pegawai_id = p_pegawai_id 
+        //     ) a
+        //     order by a.created_at desc;
+        // end;
+        // $$LANGUAGE plpgsql;";
   
-        \DB::unprepared($procedure);
+        // \DB::unprepared($procedure);
     }
 
     /**
